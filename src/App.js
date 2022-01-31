e1a1ff66b344ab5f58841c8d12bb0eb752ef1263
David Nartey <dnartey@kth.se>
2022-01-31 20:54:07 +0100

    Initial commit
    Initial commit  with user registration form

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import UserComponent from './UserComponent';
+
+function App() {
+  return (
+    <div>
+
+      <UserComponent/>
+      
+    </div>
+  );
+}
+
+export default App;
